af7fb74665b46dfa8c51589d7b69d2c02500b256
bebatut <bebatut@gmail.com>
2022-10-19 18:37:25 +0200

    Add list of repositories to parse

--- bin/extract_galaxy_tools.py
+++ bin/extract_galaxy_tools.py
@@ -9,9 +9,42 @@ def get_string_content(cf):
     '''
     Get string of the content from a ContentFile
 
-    cf: ContentFile object
+    param cf: ContentFile object
     '''
-    return str(base64.b64decode(cf.content))
+    return base64.b64decode(cf.content).decode('ascii')
+
+
+def get_tool_github_repositories(g):
+    '''
+    Get list of tool GitHub repositories to parse
+
+    param g: GitHub instance
+    '''
+    repo = g.get_user("galaxyproject").get_repo("planemo-monitor")
+    repo_list = []
+    for i in range(1, 5):
+        repo_f = repo.get_contents(f"repositories0{i}.list")
+        repo_l = get_string_content(repo_f).rstrip()
+        repo_list += repo_l.split("\n")
+    return(repo_list)
+
+
+def get_github_repo(url, g):
+    '''
+    Get a GitHub repository from an URL
+
+    param url: URL to a GitHub repository
+    param g: GitHub instance
+    '''
+    if not url.startswith("https://github.com/"):
+        raise ValueError
+    if url.endswith("/"):
+        url = url[:-1]
+    if url.endswith(".git"):
+        url = url[:-4]
+    u_split = url.split("/")
+    print(u_split)
+    return g.get_user(u_split[-2]).get_repo(u_split[-1])
 
 
 if __name__ == '__main__':
@@ -22,12 +55,14 @@ if __name__ == '__main__':
 
     # connect to GitHub
     g = Github(args.api)
-
-    # get tool GitHub repository
-    repo = g.get_user("galaxyproject").get_repo("planemo-monitor")
-    repo_list = []
-    for i in range(1, 5):
-        repo_f = repo.get_contents(f"repositories0{i}.list")
-        print(get_string_content(repo_f))
+    # get list of GitHub repositories to parse
+    repo_list = get_tool_github_repositories(g)
+    # 
+    for r in repo_list:
+        print(r)
+        if "github" not in r:
+            continue
+        repo = get_github_repo(r, g)
+        
 
 
