820454ee5a5d4035f55c5090f57d8bd48b7f3221
Bérénice Batut <bebatut@gmail.com>
2023-10-30 16:05:53 +0100

    Modify one argument, add curl for microgalaxy files in bash script

--- bin/extract_microgalaxy_tools.sh
+++ bin/extract_microgalaxy_tools.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+curl \
+        -L \
+        "https://docs.google.com/spreadsheets/d/1Nq_g-CPc8t_eC4M1NAS9XFJDflA7yE3b9hfSg3zu9L4/export?format=tsv&gid=1533244711" \
+        -o "data/microgalaxy/tools_to_keep"
+
+curl \
+        -L \
+        "https://docs.google.com/spreadsheets/d/1Nq_g-CPc8t_eC4M1NAS9XFJDflA7yE3b9hfSg3zu9L4/export?format=tsv&gid=672552331" \
+        -o "data/microgalaxy/tools_to_exclude"
+
 python bin/extract_galaxy_tools.py \
         --api $GITHUB_API_KEY \
         --output microgalaxy_tools.csv \
         --categories "data/microgalaxy/categories" \
-        --excluded "data/microgalaxy/tools_to_exclude" \
+        --exclude "data/microgalaxy/tools_to_exclude" \
         --keep "data/microgalaxy/tools_to_keep"
