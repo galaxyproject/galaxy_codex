0be309d5c67492be9288a7928161a3503783eea4
Bérénice Batut <bebatut@gmail.com>
2023-10-31 14:51:36 +0100

    Fix filtering by categories

--- bin/filter_microgalaxy_tools.sh
+++ bin/filter_microgalaxy_tools.sh
@@ -10,9 +10,12 @@ curl \
         "https://docs.google.com/spreadsheets/d/1Nq_g-CPc8t_eC4M1NAS9XFJDflA7yE3b9hfSg3zu9L4/export?format=tsv&gid=672552331" \
         -o "data/microgalaxy/tools_to_exclude"
 
+mkdir -p 'results/microgalaxy'
+
 python bin/extract_galaxy_tools.py \
         filtertools \
         --tools 'results/all_tools.csv' \
+        --filtered_tools 'results/microgalaxy/tools.csv' \
         --categories "data/microgalaxy/categories" \
         --exclude "data/microgalaxy/tools_to_exclude" \
         --keep "data/microgalaxy/tools_to_keep"
