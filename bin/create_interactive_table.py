02c7eea932c32e1ce4edd78f3652443bf85b5f14
paulzierep <paul.zierep@googlemail.com>
2023-11-01 19:10:04 +0100

    add_table_func

---
+++ bin/create_interactive_table.py
@@ -0,0 +1,89 @@
+import argparse
+
+import pandas as pd
+
+
+def generate_table(
+    tsv_path: str,
+    template_path: str,
+    output_path: str,
+) -> None:
+    df = pd.read_csv(tsv_path, sep="\t")
+
+    # need extra colum to allow expand
+    df["Expand"] = ""
+
+    df = df.fillna("")
+
+    # TODO maybe allow comunities to modify
+    columns = [
+        "Expand",
+        "Galaxy wrapper id",
+        "Galaxy wrapper version",
+        "Conda version",
+        "Conda id",
+        "Status",
+        "bio.tool id",
+        "bio.tool name",
+        "EDAM operation",
+        "EDAM topic",
+        "Description",
+        "bio.tool description",
+        "Status",
+        "Source",
+        "ToolShed categories",
+        "ToolShed id",
+        "Galaxy wrapper owner",
+        "Galaxy wrapper source",
+    ]
+
+    df = df[df["To keep"]]
+
+    df = df.loc[:, columns]
+    df = df.reindex(columns=columns)
+
+    df.to_html("temp_tools_table.html", 
+               border=0, 
+               table_id="dataframe", 
+               classes=["display", "nowrap"], 
+               index=False)
+
+    with open(template_path, "r") as template_path:
+        template = template_path.read()
+
+    with open("temp_tools_table.html", "r") as table_path:
+        table = table_path.read()
+
+    final_html_output = template.replace("COMMUNITY_TABLE", table)
+
+    with open(output_path, "w") as output:
+        output.write(final_html_output)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Create interactive table from \
+            tsv using a template, where COMMUNITY_TABLE is replaced \
+            by the rendered table"
+    )
+    parser.add_argument(
+        "--table",
+        "-ta",
+        required=True,
+        help="Path to table",
+    )
+    parser.add_argument(
+        "--template",
+        "-te",
+        required=True,
+        help="Path to html template",
+    )
+    parser.add_argument(
+        "--output",
+        "-out",
+        required=True,
+        help="Path to html output",
+    )
+
+    args = parser.parse_args()
+    generate_table(args.table, args.template, args.output)
