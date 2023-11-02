e2f236c4536a88b5bb6f614ff1fe367168122610
Bérénice Batut <bebatut@gmail.com>
2023-11-02 11:30:00 +0100

    Work on pandas code

--- bin/create_interactive_table.py
+++ bin/create_interactive_table.py
@@ -1,6 +1,31 @@
+#!/usr/bin/env python
+
 import argparse
 
 import pandas as pd
+
+
+# TODO maybe allow comunities to modify
+COLUMNS = [
+    "Expand",
+    "Galaxy wrapper id",
+    "Galaxy wrapper version",
+    "Conda version",
+    "Conda id",
+    "Status",
+    "bio.tool id",
+    "bio.tool name",
+    "EDAM operation",
+    "EDAM topic",
+    "Description",
+    "bio.tool description",
+    "Status",
+    "Source",
+    "ToolShed categories",
+    "ToolShed id",
+    "Galaxy wrapper owner",
+    "Galaxy wrapper source",
+]
 
 
 def generate_table(
@@ -8,47 +33,16 @@ def generate_table(
     template_path: str,
     output_path: str,
 ) -> None:
-    df = pd.read_csv(tsv_path, sep="\t")
-
-    # need extra colum to allow expand
-    df["Expand"] = ""
-
-    df = df.fillna("")
-
-    # TODO maybe allow comunities to modify
-    columns = [
-        "Expand",
-        "Galaxy wrapper id",
-        "Galaxy wrapper version",
-        "Conda version",
-        "Conda id",
-        "Status",
-        "bio.tool id",
-        "bio.tool name",
-        "EDAM operation",
-        "EDAM topic",
-        "Description",
-        "bio.tool description",
-        "Status",
-        "Source",
-        "ToolShed categories",
-        "ToolShed id",
-        "Galaxy wrapper owner",
-        "Galaxy wrapper source",
-    ]
-
-    df = df[df["To keep"]]
-
-    df = df.loc[:, columns]
-    df = df.reindex(columns=columns)
-
-    df.to_html("temp_tools_table.html", border=0, table_id="dataframe", classes=["display", "nowrap"], index=False)
+    df = (pd.read_csv(tsv_path, sep="\t")
+        .assign(Expand=lambda df: "")
+        .fillna(""))
+    df = (df[df["To keep"]]
+        .loc[:, COLUMNS]
+        .reindex(columns=COLUMNS))
+    table = df.to_html(border=0, table_id="dataframe", classes=["display", "nowrap"], index=False)
 
     with open(template_path) as template_file:
         template = template_file.read()
-
-    with open("temp_tools_table.html") as table_file:
-        table = table_file.read()
 
     final_html_output = template.replace("COMMUNITY_TABLE", table)
 
@@ -59,26 +53,26 @@ def generate_table(
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Create interactive table from \
-            tsv using a template, where COMMUNITY_TABLE is replaced \
+            TSV file using a template, where COMMUNITY_TABLE is replaced \
             by the rendered table"
     )
     parser.add_argument(
         "--table",
         "-ta",
         required=True,
-        help="Path to table",
+        help="Path to TSV file with tools",
     )
     parser.add_argument(
         "--template",
         "-te",
         required=True,
-        help="Path to html template",
+        help="Path to HTML template",
     )
     parser.add_argument(
         "--output",
         "-out",
         required=True,
-        help="Path to html output",
+        help="Path to HTML output",
     )
 
     args = parser.parse_args()
