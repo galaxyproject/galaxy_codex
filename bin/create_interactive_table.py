e2cc2c24beaab88576fbb49575fa628447383d44
Bérénice Batut <bebatut@gmail.com>
2023-11-02 13:35:45 +0100

    Fix create table script to run with imaging

--- bin/create_interactive_table.py
+++ bin/create_interactive_table.py
@@ -33,7 +33,8 @@ def generate_table(
     output_path: str,
 ) -> None:
     df = pd.read_csv(tsv_path, sep="\t").assign(Expand=lambda df: "").fillna("")
-    df = df[df["To keep"]].loc[:, COLUMNS].reindex(columns=COLUMNS)
+    df["To keep"] = df["To keep"].replace("", True)
+    df = df.query("`To keep`").loc[:, COLUMNS].reindex(columns=COLUMNS)
     table = df.to_html(border=0, table_id="dataframe", classes=["display", "nowrap"], index=False)
 
     with open(template_path) as template_file:
