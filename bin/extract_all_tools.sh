b602b6cc435705d4328d13b4166ed71af383027a
Bérénice Batut <bebatut@gmail.com>
2023-11-02 13:50:57 +0100

    Add table with all tools

--- bin/extract_all_tools.sh
+++ bin/extract_all_tools.sh
@@ -6,3 +6,8 @@ python bin/extract_galaxy_tools.py \
         extractools \
         --api $GITHUB_API_KEY \
         --all_tools 'results/all_tools.tsv'
+
+python bin/create_interactive_table.py \
+        --table "results/all_tools.tsv" \
+        --template "data/interactive_table_template.html" \
+        --output "results/index.html"
