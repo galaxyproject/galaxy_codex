8e3c61921b39f24901a26b2843282b30269e01a7
Bérénice Batut <bebatut@gmail.com>
2023-11-02 11:31:48 +0100

    Merge bash community scripts and update microgalaxy

--- bin/create_interactive_table.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p 'results/microgalaxy'
-
-python bin/create_interactive_table.py \
-        --table 'results/microgalaxy/tools.tsv' \
-        --template 'data/microgalaxy/datatable_template.html' \
-        --output 'results/microgalaxy/index.html'
