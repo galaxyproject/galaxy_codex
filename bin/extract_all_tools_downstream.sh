b28b3a07c07e0c4fafcceab1aed23f31aa057845
paulzierep <paul.zierep@googlemail.com>
2024-01-10 10:58:32 +0100

    Ci prod (#35)
    * action in main to manually dispatch
    
    * update
    
    * update
    
    * update
    
    * update
    
    * All tools bot
    
    * full wf
    
    * update
    
    * filter community tool bot
    
    * filter communities bot
    
    * test flter
    
    * filter communities bot
    
    * trigger pages only when new results are created
    
    * Update .github/workflows/static.yml
    
    Co-authored-by: Nicola Soranzo <nicola.soranzo@gmail.com>
    
    * fetch tools stepwise with CI
    
    * update CI
    
    * updat CI
    
    * update CI
    
    * update CI
    
    * update CI and get more verbose tool logs
    
    * add a test case to check what up with the CI
    
    * allow to run the test ever
    
    * try test with other api key
    
    * next try
    
    * as secrets
    
    * fetch all tools bot
    
    * merge test
    
    * cat in CI
    
    * CI all tools
    
    * CI test
    
    * force push
    
    * fetch all tools bot  - step merge
    
    * test git merge
    
    * more tests
    
    * cover staged change as well
    
    * fetch all tools bot  - step fetch
    
    * CI test
    
    * fetch all tools bot  - step fetch
    
    * update fetch and filter CI
    
    * removed results from test runs
    
    * fetch all tools bot  - step fetch
    
    * try pull merge with rebase
    
    * merge then push
    
    * test
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step fetch
    
    * CI update
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step merge
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step merge
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step merge
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step merge
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step merge
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step fetch
    
    * fetch all tools bot  - step merge
    
    * Add nsoranzo review
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Nicola Soranzo <nicola.soranzo@gmail.com>

--- bin/extract_all_tools_downstream.sh
+++ bin/extract_all_tools_downstream.sh
@@ -2,11 +2,6 @@
 
 mkdir -p 'results/'
 
-python bin/extract_galaxy_tools.py \
-        extractools \
-        --api $GITHUB_API_KEY \
-        --all_tools 'results/all_tools.tsv'
-
 python bin/create_interactive_table.py \
         --table "results/all_tools.tsv" \
         --template "data/interactive_table_template.html" \
---
+++ bin/extract_all_tools_test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+mkdir -p 'results/'
+
+output="results/${1}_tools.tsv"
+
+python bin/extract_galaxy_tools.py \
+        extractools \
+        --api $GITHUB_API_KEY \
+        --all_tools $output \
+        --planemorepository $1 \
+        --test
+
