ecd9ec646428c1b4eb120f760c5b7cec37bb29a1
paulzierep <paul.zierep@googlemail.com>
2024-03-18 16:44:51 +0100

    fix

--- bin/extract_galaxy_tools.py
+++ bin/extract_galaxy_tools.py
@@ -638,7 +638,7 @@ def extract_public_galaxy_servers_tools() -> Dict:
     TODO: run get_public_galaxy_servers.py as CI
     """
 
-    df = pd.read_csv("PUBLIC_GALAXY_SERVERS")
+    df = pd.read_csv(PUBLIC_GALAXY_SERVERS)
     to_process = pd.Series(df["urls"].values, index=df["Name"]).to_dict()
 
     return to_process
