81d5dae54366f9c50646687f3db46323f640271e
paulzierep <paul.zierep@googlemail.com>
2024-06-04 12:38:44 +0200

    linting

--- bin/extract_galaxy_tools.py
+++ bin/extract_galaxy_tools.py
@@ -21,7 +21,6 @@ import yaml
 from github import Github
 from github.ContentFile import ContentFile
 from github.Repository import Repository
-
 from owlready2 import get_ontology
 
 # Config variables
