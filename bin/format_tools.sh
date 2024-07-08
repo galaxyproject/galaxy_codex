c4f0c54b9b21d8db61dcd3815985d0b80cdf931d
Bérénice Batut <berenice.batut@gmail.com>
2024-07-08 16:54:02 +0200

    Make wordcloud script more generic

--- bin/format_tools.sh
+++ bin/format_tools.sh
@@ -9,6 +9,7 @@ python bin/create_interactive_table.py \
 
 python bin/create_wordcloud.py \
         --table "results/all_tools.tsv" \
+        --name_col "Galaxy wrapper id" \
+        --stat_col "No. of tool users (2022-2023) (usegalaxy.eu)" \
         --wordcloud_mask "data/usage_stats/wordcloud_mask.png" \
         --output "results/all_tools_wordcloud.png" \
-        --stats_column "No. of tool users (2022-2023) (usegalaxy.eu)"
