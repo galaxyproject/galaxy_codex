1580119bff20b68abb271a64ed0de60fd487b324
Cameron Hyde <chyde@neoformit.com>
2024-08-09 07:27:16 +1000

    Black/isort formatting

--- subdomains/scripts/tools_au.py
+++ subdomains/scripts/tools_au.py
@@ -11,50 +11,49 @@ parse_tools_to_produce_yml_files.py script, they should look like:
 See subdomains/singlecell/tool_panel for example.
 """
 
-import yaml
 from pathlib import Path
+
+import yaml
 
 WDIR = Path(__file__).parent
 EXCLUDE_KEYS = [
-    'install_repository_dependencies',
-    'tool_panel_section_label',
+    "install_repository_dependencies",
+    "tool_panel_section_label",
 ]
-OUTPUT_FILE = WDIR / 'usegalaxy.org.au.yml'
+OUTPUT_FILE = WDIR / "usegalaxy.org.au.yml"
 
 
 def parse():
     """Parse tools list from YAML files."""
-    data = {'tools': []}
-    for f in WDIR.glob('*.yml.lock'):
-        if not f.name.endswith('.yml.lock'):
+    data = {"tools": []}
+    for f in WDIR.glob("*.yml.lock"):
+        if not f.name.endswith(".yml.lock"):
             continue
         print(f"Extracting data from {f}...")
         with open(f) as handle:
             section_data = yaml.safe_load(handle)
-        section_data['tools'] = [
-            _transcribe_to_au(x) for x in section_data['tools']
-        ]
+        section_data["tools"] = [_transcribe_to_au(x) for x in section_data["tools"]]
         for key in EXCLUDE_KEYS:
             section_data.pop(key)
-        data['tools'] += section_data['tools']
-    for key in ('install_resolver_dependencies', 'install_tool_dependencies'):
+        data["tools"] += section_data["tools"]
+    for key in ("install_resolver_dependencies", "install_tool_dependencies"):
         data[key] = section_data[key]
     write_data(data)
-    print(f'Data transcribed and written to {OUTPUT_FILE}')
+    print(f"Data transcribed and written to {OUTPUT_FILE}")
 
 
 def _transcribe_to_au(tool):
     """Convert keys to AU installation format."""
-    tool['tool_panel_section_label'] = 'Single-Cell'
-    tool.pop('tool_panel_section_id')
+    tool["tool_panel_section_label"] = "Single-Cell"
+    tool.pop("tool_panel_section_id")
     return tool
 
 
 def write_data(data):
     """Write data to YAML output file."""
-    with open(OUTPUT_FILE, 'w') as handle:
+    with open(OUTPUT_FILE, "w") as handle:
         yaml.dump(data, handle, default_flow_style=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parse()
