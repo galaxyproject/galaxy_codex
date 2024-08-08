3c31d7fd2cc19a40f4535f64491d144aafcbc26c
Cameron Hyde <chyde@neoformit.com>
2024-08-09 07:12:37 +1000

    Add docstrings

--- subdomains/scripts/tools_au.py
+++ subdomains/scripts/tools_au.py
@@ -1,6 +1,14 @@
 """Convert YAML files for install on AU.
 
-Assumes that wdir contains tool yaml files.
+Assumes that wdir contains tool yaml files produced by the
+parse_tools_to_produce_yml_files.py script, they should look like:
+<tool_section_1>.yml
+<tool_section_1>.yml.lock
+<tool_section_2>.yml
+<tool_section_2>.yml.lock
+...
+
+See subdomains/singlecell/tool_panel for example.
 """
 
 import yaml
@@ -15,6 +23,7 @@ OUTPUT_FILE = WDIR / 'usegalaxy.org.au.yml'
 
 
 def parse():
+    """Parse tools list from YAML files."""
     data = {'tools': []}
     for f in WDIR.glob('*.yml.lock'):
         if not f.name.endswith('.yml.lock'):
@@ -35,12 +44,14 @@ def parse():
 
 
 def _transcribe_to_au(tool):
+    """Convert keys to AU installation format."""
     tool['tool_panel_section_label'] = 'Single-Cell'
     tool.pop('tool_panel_section_id')
     return tool
 
 
 def write_data(data):
+    """Write data to YAML output file."""
     with open(OUTPUT_FILE, 'w') as handle:
         yaml.dump(data, handle, default_flow_style=False)
 
