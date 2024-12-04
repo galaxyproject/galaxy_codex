78bdb155d7430529a3b81ab0707a8559b24b35d2
Bérénice Batut <bebatut@gmail.com>
2024-12-04 12:02:38 +0100

    Fix tool IDs for CI (#240)

--- sources/bin/shared.py
+++ sources/bin/shared.py
@@ -83,7 +83,7 @@ def read_suite_per_tool_id(tool_fp: str) -> Dict:
     tool_suites = load_json(tool_fp)
     tools = {}
     for suite in tool_suites:
-        for tool in suite["Suite ID"]:
+        for tool in suite["Tool IDs"]:
             tools[tool] = {
                 "Suite ID": suite["Suite ID"],
                 "Suite owner": suite["Suite owner"],
