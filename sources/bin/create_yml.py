3f7a5be8a04f4358797042dc0980281885304b5e
Johan Gustafsson <johan@biocommons.org.au>
2024-12-05 16:42:39 +1030

    update create_yml.py to add workflows

--- sources/bin/create_yml.py
+++ sources/bin/create_yml.py
@@ -3,7 +3,7 @@ import yaml
 import json
 import re
 
-def create_yml(data_source: str, yml_output_path: str):
+def create_tool_yml(data_source: str, yml_output_path: str):
 
     with open(data_source, mode="r", encoding="utf-8") as file:
         data = json.load(file)
@@ -26,4 +26,14 @@ def create_yml(data_source: str, yml_output_path: str):
     with open(yml_output_path, 'w') as file:
         yaml.dump(data, file, default_flow_style=False)
 
-create_yml(data_source = "./communities/all/resources/tools.json", yml_output_path = "./docs/_data/tools.yml")
+create_tool_yml(data_source = "./communities/all/resources/tools.json", yml_output_path = "./docs/_data/tools.yml")
+
+def create_yml(data_source: str, yml_output_path: str):
+
+    with open(data_source, mode="r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    with open(yml_output_path, 'w') as file:
+        yaml.dump(data, file, default_flow_style=False)
+
+create_yml(data_source = "./communities/all/resources/workflows.json", yml_output_path = "./docs/_data/workflows.yml")
