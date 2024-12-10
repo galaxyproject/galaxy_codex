a857c31434c17c60387dfad9975e8403b92b1a07
Johan Gustafsson <johan@biocommons.org.au>
2024-12-10 11:28:53 +1030

    update create_yml.py and add tutorials.yml

--- sources/bin/create_yml.py
+++ sources/bin/create_yml.py
@@ -37,3 +37,25 @@ def create_yml(data_source: str, yml_output_path: str):
         yaml.dump(data, file, default_flow_style=False)
 
 create_yml(data_source = "./communities/all/resources/workflows.json", yml_output_path = "./docs/_data/workflows.yml")
+
+
+def create_training_yml(data_source: str, yml_output_path: str, fields: list):
+
+    with open(data_source, mode="r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    updated_training_data = {}
+
+    for training in data:
+        id = training["id"]
+        updated_training_data[id] = {}
+        for field in fields:
+            if field in training:
+                updated_training_data[id][field] = training[field]
+
+    with open(yml_output_path, 'w') as file:
+        yaml.dump(updated_training_data, file, default_flow_style=False)
+
+required_fields = ["title", "hands_on", "url", "slides", "mod_date", "pub_date", "version", "short_tools", "exact_supported_servers","inexact_supported_servers", "topic_name_human", "video", "edam_topic", "edam_operation", "feedback_number", "feedback_mean_note", "visitors", "pageviews", "visit_duration", "video_versions", "video_view"]
+
+create_training_yml(data_source = "./communities/all/resources/tutorials.json", yml_output_path = "./docs/_data/tutorials.yml", fields = required_fields)
