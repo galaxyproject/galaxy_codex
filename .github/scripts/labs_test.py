9576c93313a3dfdb96f873f075c978083ba09701
Cameron Hyde <chyde@neoformit.com>
2024-12-24 08:30:12 +1000

    Move test logic to PR workflow

--- .github/scripts/labs_test.py
+++ .github/scripts/labs_test.py
@@ -6,10 +6,10 @@ in a PR comment.
 
 import os
 import sys
-from github import Github
 from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
 
+OUTPUT_DIR = "comments"
 COMMENT_TITLE_TEMPLATE = "Preview changes to {lab_name} Lab <!--=-->"
 URL_TEMPLATE = (
     "https://labs.usegalaxy.org.au"
@@ -25,49 +25,18 @@ TRY_FILES = [
 ]
 
 # Environment variables from GitHub Actions
-PR_NUMBER = int(os.environ["PR_NUMBER"])
 BRANCH_NAME = os.environ["BRANCH_NAME"]
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-BASE_REPO = os.getenv("BASE_REPO")
 HEAD_REPO = os.getenv("HEAD_REPO")
 
 
-def get_comment(pull_request, id_string):
-    """Fetches PR comments and scans for the COMMENT_TITLE_TEMPLATE."""
-    for comment in pull_request.get_issue_comments():
-        if id_string in comment.body:
-            return comment
-    return None
-
-
-def create_or_update_comment(lab_name, body_md):
-    """Creates or updates a comment for the given lab name.
-
-    Checks for an existing comment by looking for the COMMENT_TITLE_TEMPLATE
-    in existing comments.
-    """
-    divider = "\n\n" + '-' * 80 + '\n\n'
-    print("Posting comment:", divider, body_md.strip(' \n'), divider)
-    title_string = COMMENT_TITLE_TEMPLATE.format(lab_name=lab_name)
-    gh = Github(GITHUB_TOKEN)
-    print("Getting base repo:", BASE_REPO)
-    repo = gh.get_repo(BASE_REPO)
-    pull_request = repo.get_pull(PR_NUMBER)
-    comment = get_comment(pull_request, title_string)
-    if comment:
-        comment.edit(body_md)
-    else:
-        pull_request.create_issue_comment(body_md)
-
-
-def post_lab_links(lab_name):
+def test_lab(lab_name):
     """Iterate through each YAML root file for the given lab name.
     For files that exist, build a URL for that Lab page, check the HTTP status
     code and post the URLs with pass/fail status as a comment on the PR.
     """
     success = True
     title_string = COMMENT_TITLE_TEMPLATE.format(lab_name=lab_name)
-    comment = f"### {title_string}\n\n"
+    comment_md = f"### {title_string}\n\n"
     test_paths = [
         f'communities/{lab_name}/lab/{f}'
         for f in TRY_FILES
@@ -89,17 +58,23 @@ def post_lab_links(lab_name):
         except URLError as exc:
             line = f"- ❌ {filename} [URL ERROR]: {url}\n\n```\n{exc}\n```"
             success = False
-        comment += line
+        comment_md += line
 
     if not success:
-        comment += (
+        comment_md += (
             "\n\n"
             "🚨 One or more Lab pages are returning an error. "
             "Please follow the link to see details of the issue."
         )
 
-    create_or_update_comment(lab_name, comment)
+    write_comment(lab_name, comment_md)
     return success
+
+
+def write_comment(lab_name, md):
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    with open(f'{OUTPUT_DIR}/{lab_name}.md', 'w') as f:
+        f.write(md)
 
 
 def http_status_for(url):
@@ -133,7 +108,7 @@ def main():
                 print(f"Detected change to {lab_name} Lab in file: {path}")
                 print(f"Posting link for {lab_name}...")
                 directories.append(lab_name)
-                result = post_lab_links(lab_name)
+                result = test_lab(lab_name)
                 success = success and result
         else:
             print(f"Ignoring changes to {path}: not in a lab directory")
