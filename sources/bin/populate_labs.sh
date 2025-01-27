8e87e88996188e4c6c3dffaa031badf9a44b1623
Bérénice Batut <berenice.batut@gmail.com>
2025-01-27 10:22:20 +0100

    Modify to populate lab thematic section with only tutorials

--- sources/bin/populate_labs.sh
+++ sources/bin/populate_labs.sh
@@ -6,42 +6,22 @@ python sources/bin/populate_labs_tools.py \
     --tool_tsv communities/microgalaxy/resources/curated_tools.tsv \
     --tool_yml communities/microgalaxy/lab/sections/4_tools.yml
 
-python sources/bin/populate_labs_workflows_tutorials.py \
+python sources/bin/populate_labs_tutorials.py \
     --tsv communities/microgalaxy/resources/tutorials.tsv \
-    --yml communities/microgalaxy/lab/sections/tutorials_isolates_snippet.yml \
-    -tc Title \
-    -dc Title \
-    -blc Link \
-    -fc Topic \
-    -fi Microbiome \
-    -fl exclude
+    --yml communities/microgalaxy/lab/sections/2_microbial_isolates.yml \
+    --title-column Title \
+    --description-column Title \
+    --button-link-column Link \
+    --filter-column Topic \
+    --filter Microbiome \
+    --filter-logic exclude
 
-python sources/bin/populate_labs_workflows_tutorials.py \
+python sources/bin/populate_labs_tutorials.py \
     --tsv communities/microgalaxy/resources/tutorials.tsv \
-    --yml communities/microgalaxy/lab/sections/tutorials_microbiome_snippet.yml \
-    -tc Title \
-    -dc Title \
-    -blc Link \
-    -fc Topic \
-    -fi Microbiome \
-    -fl include
-
-python sources/bin/populate_labs_workflows_tutorials.py \
-    --tsv communities/microgalaxy/resources/workflows.tsv \
-    --yml communities/microgalaxy/lab/sections/workflows_isolate_snippet.yml \
-    -tc Name \
-    -dc Name \
-    -blc Link \
-    -fc Tags \
-    -fi "metagenomics, metatranscriptomics" \
-    -fl exclude
-
-python sources/bin/populate_labs_workflows_tutorials.py \
-    --tsv communities/microgalaxy/resources/workflows.tsv \
-    --yml communities/microgalaxy/lab/sections/workflows_microbiome_snippet.yml \
-    -tc Name \
-    -dc Name \
-    -blc Link \
-    -fc Tags \
-    -fi "metagenomics, metatranscriptomics" \
-    -fl include
+    --yml communities/microgalaxy/lab/sections/3_microbiome.yml \
+    --title-column Title \
+    --description-column Title \
+    --button-link-column Link \
+    --filter-column Topic \
+    --filter Microbiome \
+    --filter-logic include
