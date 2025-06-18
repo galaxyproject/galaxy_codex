b25ce22a30ae17ec0ddfecb076fe4b65db3bb125
Solenne Correard <54953390+scorreard@users.noreply.github.com>
2025-06-18 14:16:40 +0200

    Fix linting issue in extract_galaxy_workflows.py

--- sources/bin/extract_galaxy_workflows.py
+++ sources/bin/extract_galaxy_workflows.py
@@ -280,7 +280,7 @@ class Workflows:
             if wf["published"] and wf["importable"] and not wf["deleted"] and not wf["hidden"]:
                 count += 1
                 # The following workflow (id=f65a2f2a19bea880) caused the request to crash, it was reported to the Galaxy team, in the meantime, we are skipping it
-                if wf['id'] == "f65a2f2a19bea880":
+                if wf["id"] == "f65a2f2a19bea880":
                     continue
                 server_wf = shared.get_request_json(
                     f"{server}/api/workflows/{wf['id']}",
