e6f7602701acca413c6aded4c3c27d5d191d3d68
Solenne Correard <54953390+scorreard@users.noreply.github.com>
2025-06-25 14:46:13 +0200

    Update populate_labs.sh

--- sources/bin/populate_labs.sh
+++ sources/bin/populate_labs.sh
@@ -7,69 +7,58 @@
 ## Change the community variable to match your community
 COMMUNITY="biodiversity"
 
+##Sections
 mkdir -p communities/$COMMUNITY/lab/sections/
 
-#Tools
-#This creates a tools folder (in the communauty lab folder) with one yaml files per tool
-python sources/bin/extract_galaxy_tools.py \
-    getLabTools \
-    --curated communities/$COMMUNITY/resources/curated_tools.tsv \
-    --tools communities/$COMMUNITY/lab/tools
+#Each section must be reviewed the first time before publication!!
 
-#This creates or update the tool file
-tools_file="communities/$COMMUNITY/lab/sections/4_tools.yml"
-#Copy tool file from another community if they do not yet exist
-if [[ ! -e $tools_file ]]; then
-    cat <<EOF > "$tools_file"
-id: Tools
-title: Community curated tools
-tabs: []
-EOF
+# Data import and preparation
+#This is general enough to be the same between several communities.
+#Some sections are commented at the bottom and are potential addition for your community
+data_import_export_section="communities/$COMMUNITY/lab/sections/1_data_import_and_preparation.yml
+if [[ ! -e $data_import_export_section ]]; then
+   cp sources/bin/sections_templates/1_data_import_and_preparation.yml $data_import_export_section
 fi
 
+#Tools
+#This creates or update the tool file
+tools_section="communities/$COMMUNITY/lab/sections/2_tools.yml"
+#Copy tools file from the template file if it does not yet exist
+if [[ ! -e $tools_section ]]; then
+    cp sources/bin/sections_templates/2_tools.yml $tools_section
+fi
 #Update the tool file
 python sources/bin/extract_galaxy_tools.py \
     popLabSection \
     --curated communities/$COMMUNITY/resources/curated_tools.tsv \
-    --lab $tools_file
+    --lab $tools_section
 
 #Workflows
-workflows_file="communities/$COMMUNITY/lab/sections/5_workflows.yml"
-if [[ ! -e $workflows_file ]]; then
-    cat <<EOF > "$workflows_file"
-id: Workflows
-title: Community workflows
-tabs: []
-EOF
+workflows_section="communities/$COMMUNITY/lab/sections/3_workflows.yml"
+#Copy workflows file from the template file if it does not yet exist
+if [[ ! -e $workflows_section ]]; then
+    cp sources/bin/sections_templates/3_workflows.yml $workflows_section
 fi
 
 #Script to include the expected workflows
 python sources/bin/extract_galaxy_workflows.py \
     popLabSection \
     --curated communities/$COMMUNITY/resources/curated_workflows.json \
-    --lab $workflows_file
+    --lab $workflows_section
 
 
 #Tutorials
-tutorials_file="communities/$COMMUNITY/lab/sections/6_tutorials.yml"
+tutorials_section="communities/$COMMUNITY/lab/sections/4_tutorials.yml"
 
-if [[ ! -e "$tutorials_file" ]]; then
-    # Create the file with correct indentation
-    cat <<EOF > "$tutorials_file"
-id: Tutorials
-title: Community tutorials
-tabs:
-  - id: tutorials
-EOF
+#Copy tutorials file from the template file if it does not yet exist
+if [[ ! -e $tutorials_section ]]; then
+    cp sources/bin/sections_templates/4_tutorials.yml $tutorials_section
 fi
-
-echo "tutorials_file"
-head $tutorials_file
 
 #Update the tutorial file
 python sources/bin/populate_labs_tutorials.py \
     --tsv communities/$COMMUNITY/resources/tutorials.tsv \
-    --yml $tutorials_file \
+    --yml $tutorials_section \
     --title-column Title \
     --description-column Title \
     --button-link-column Link \
@@ -77,13 +66,32 @@ python sources/bin/populate_labs_tutorials.py \
     --filter $COMMUNITY \
     --filter-logic exclude
 
-#awk '
-#/^- id: best_practices/ {flag=1; next}
-#/^- id:/ {flag=0}
-#!flag
-#' communities/biodiversity/lab/sections/6_tutorials.yml > tmp.yml && mv tmp.yml communities/biodiversity/lab/sections/6_tutorials.yml
+#Support and Help
+#This is general enough to be the same between several communities.
+#Some sections are commented at the bottom and are potential addition for your community
+support_help_section="communities/$COMMUNITY/lab/sections/5_support_and_help.yml
+if [[ ! -e $support_help_section ]]; then
+   cp sources/bin/sections_templates/5_support_and_help.yml $support_help_section
+fi
 
-# Below are scripts used by the microgalaxy community:
+#Comunity
+#This need manual review to add links that are community specific (matrix, etc)
+#Some sections are commented at the bottom and are potential addition for your community
+community_section="communities/$COMMUNITY/lab/sections/6_community.yml
+if [[ ! -e $community_section ]]; then
+   cp sources/bin/sections_templates/6_community.yml $community_section
+fi
+
+
+##Tools
+#This creates a tools folder (in the communauty lab folder) with one yaml files per tool
+python sources/bin/extract_galaxy_tools.py \
+    getLabTools \
+    --curated communities/$COMMUNITY/resources/curated_tools.tsv \
+    --tools communities/$COMMUNITY/lab/tools
+
+
+# Below are scripts used by the microgalaxy community to create other sections:
 #python sources/bin/populate_labs_tutorials.py \
 #    --tsv communities/microgalaxy/resources/tutorials.tsv \
 #    --yml communities/microgalaxy/lab/sections/2_microbial_isolates.yml \
