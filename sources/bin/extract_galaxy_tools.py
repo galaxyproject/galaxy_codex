6c374bac011a9b3b4d273a136a94ed9d40f78bac
Solenne Correard <54953390+scorreard@users.noreply.github.com>
2025-11-28 10:40:45 +0100

    lint

--- sources/bin/extract_galaxy_tools.py
+++ sources/bin/extract_galaxy_tools.py
@@ -876,7 +876,7 @@ def extract_top_tools_per_category(
 
     # Step 0 : Add the string "No associated EDAM operation" in all the empty cells of the "EDAM operations" column
     tools["EDAM operations"] = tools["EDAM operations"].fillna("No associated EDAM operation").astype(str)
-    
+
     # Step 1: Split the categories into separate rows and strip whitespace
     df = tools.assign(Category=tools["EDAM operations"].str.split(",")).explode("Category")
     df["Category"] = df["Category"].str.strip()  # Strip whitespace
