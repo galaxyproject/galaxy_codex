ff4d4c8bccd9b232672fc12196abfdeab55ab3be
Bérénice Batut <berenice.batut@gmail.com>
2026-01-05 10:09:50 +0100

    Run tests in bash scripts on all commands

--- sources/bin/extract_all_tutorials.sh
+++ sources/bin/extract_all_tutorials.sh
@@ -4,8 +4,8 @@ if [ ! -z $1 ]
 then
         python sources/bin/extract_gtn_tutorials.py \
                 extract \
-                --all "communities/all/resources/test_tutorials.json" \
-                --tools "communities/all/resources/test_tools.json" \
+                --all "communities/all/resources/tutorials.json" \
+                --tools "communities/all/resources/tools.json" \
                 --api $PLAUSIBLE_API_KEY \
                 --test
 else
@@ -14,19 +14,17 @@ else
                 --all "communities/all/resources/tutorials.json" \
                 --tools "communities/all/resources/tools.json" \
                 --api $PLAUSIBLE_API_KEY
-
-        python sources/bin/extract_gtn_tutorials.py \
-                filter \
-                --all "communities/all/resources/tutorials.json" \
-                --yml "communities/all/resources/tutorials.yml" \
-                --filtered "communities/all/resources/tutorials.tsv"
-
-        # ln -s "./../../communities/all/resources/tutorials.yml" "./_data/tutorials.yml"
-  
-        python sources/bin/create_interactive_table.py \
-                --input "communities/all/resources/tutorials.tsv" \
-                --template "sources/data/interactive_table_template.html" \
-                --output "communities/all/resources/tutorials.html"          
 fi
+
+python sources/bin/extract_gtn_tutorials.py \
+        filter \
+        --all "communities/all/resources/tutorials.json" \
+        --yml "communities/all/resources/tutorials.yml" \
+        --filtered "communities/all/resources/tutorials.tsv"
+
+python sources/bin/create_interactive_table.py \
+        --input "communities/all/resources/tutorials.tsv" \
+        --template "sources/data/interactive_table_template.html" \
+        --output "communities/all/resources/tutorials.html"
 
 
--- sources/bin/get_community_tools.sh
+++ sources/bin/get_community_tools.sh
@@ -3,76 +3,51 @@
 # stop on error
 set -e 
 
-if [[ ! -z $1  && $1 == "test" ]]; then
-        python sources/bin/extract_galaxy_tools.py \
-                filter \
-                --all "communities/all/resources/test_tools.json" \
-                --categories "communities/microgalaxy/metadata/categories" \
-                --filtered "communities/microgalaxy/resources/tools_filtered_by_ts_categories.json" \
-                --status "communities/microgalaxy/metadata/tool_status.tsv"
-   
-        python sources/bin/extract_galaxy_tools.py \
-                curate \
-                --filtered "communities/microgalaxy/resources/tools_filtered_by_ts_categories.json" \
-                --status "communities/microgalaxy/metadata/tool_status.tsv" \
-                --curated "communities/microgalaxy/resources/curated_tools.tsv" \
-                --wo-biotools "communities/microgalaxy/resources/curated_tools_wo_biotools.tsv" \
-                --w-biotools "communities/microgalaxy/resources/curated_tools_w_biotools.tsv"\
-                --yml "communities/microgalaxy/resources/curated_tools.yml"
+for com_data_fp in communities/* ; do
+        if [[ -d "$com_data_fp" && ! -L "$com_data_fp" ]]; then
+                community=`basename "$com_data_fp"`
 
-        mkdir -p _data/communities/microgalaxy/
-        ln -sf ../../../communities/microgalaxy/resources/curated_tools.yml _data/communities/microgalaxy/curated_tools.yml
+                if [[ ! -z $1  && $1 == "test" && "$community" != "microgalaxy" ]]; then
+                        continue
+                fi;
 
-        python sources/bin/create_wordcloud.py \
-                --input  "communities/microgalaxy/resources/curated_tools.tsv" \
-                --name-col "Suite ID" \
-                --stat-col "Suite runs (last 5 years) on main servers" \
-                --wordcloud_mask "sources/data/usage_stats/wordcloud_mask.png" \
-                --output "communities/microgalaxy/resources/tools_wordcloud.png"
+                if [[ "$community" != "all" && -f "communities/$community/metadata/categories" ]]; then
+                        echo "$community";
+                        mkdir -p "communities/$community/resources"
 
-else
-        for com_data_fp in communities/* ; do
-                if [[ -d "$com_data_fp" && ! -L "$com_data_fp" ]]; then
-                        community=`basename "$com_data_fp"`
+                        python sources/bin/extract_galaxy_tools.py \
+                                filter \
+                                --all "communities/all/resources/tools.json" \
+                                --categories "communities/$community/metadata/categories" \
+                                --filtered "communities/$community/resources/tools_filtered_by_ts_categories.json" \
+                                --status "communities/$community/metadata/tool_status.tsv"
 
-                        if [[ "$community" != "all" && -f "communities/$community/metadata/categories" ]]; then
-                                echo "$community";
-                                mkdir -p "communities/$community/resources"
-
+                        if [[ -e "communities/$community/metadata/tool_status.tsv" ]]; then
                                 python sources/bin/extract_galaxy_tools.py \
-                                        filter \
-                                        --all "communities/all/resources/tools.json" \
-                                        --categories "communities/$community/metadata/categories" \
+                                        curate \
                                         --filtered "communities/$community/resources/tools_filtered_by_ts_categories.json" \
-                                        --status "communities/$community/metadata/tool_status.tsv"
+                                        --status "communities/$community/metadata/tool_status.tsv" \
+                                        --curated "communities/$community/resources/curated_tools.tsv" \
+                                        --wo-biotools "communities/$community/resources/curated_tools_wo_biotools.tsv" \
+                                        --w-biotools "communities/$community/resources/curated_tools_w_biotools.tsv"\
+                                        --yml "communities/$community/resources/curated_tools.yml"
 
-                                if [[ -e "communities/$community/metadata/tool_status.tsv" ]]; then
-                                        python sources/bin/extract_galaxy_tools.py \
-                                                curate \
-                                                --filtered "communities/$community/resources/tools_filtered_by_ts_categories.json" \
-                                                --status "communities/$community/metadata/tool_status.tsv" \
-                                                --curated "communities/$community/resources/curated_tools.tsv" \
-                                                --wo-biotools "communities/$community/resources/curated_tools_wo_biotools.tsv" \
-                                                --w-biotools "communities/$community/resources/curated_tools_w_biotools.tsv"\
-                                                --yml "communities/$community/resources/curated_tools.yml"
+                                if [[ -e "communities/$community/resources/curated_tools.yml" ]]; then
+                                        mkdir -p _data/communities/$community/
+                                        ln -sf ../../../communities/$community/resources/curated_tools.yml _data/communities/$community/curated_tools.yml
+                                fi;
 
-                                        if [[ -e "communities/$community/resources/curated_tools.yml" ]]; then
-                                                mkdir -p _data/communities/$community/
-                                                ln -sf ../../../communities/$community/resources/curated_tools.yml _data/communities/$community/curated_tools.yml
-                                        fi;
-
-                                        if [[ -e "communities/$community/resources/curated_tools.tsv" ]]; then
-                                                python sources/bin/create_wordcloud.py \
-                                                        --input "communities/$community/resources/curated_tools.tsv" \
-                                                        --name-col "Suite ID" \
-                                                        --stat-col "Suite runs (last 5 years) on main servers" \
-                                                        --wordcloud_mask "sources/data/usage_stats/wordcloud_mask.png" \
-                                                        --output "communities/$community/resources/tools_wordcloud.png"
-                                        fi;
+                                if [[ -e "communities/$community/resources/curated_tools.tsv" ]]; then
+                                        python sources/bin/create_wordcloud.py \
+                                                --input "communities/$community/resources/curated_tools.tsv" \
+                                                --name-col "Suite ID" \
+                                                --stat-col "Suite runs (last 5 years) on main servers" \
+                                                --wordcloud_mask "sources/data/usage_stats/wordcloud_mask.png" \
+                                                --output "communities/$community/resources/tools_wordcloud.png"
                                 fi;
                         fi;
                 fi;
-        done
-fi
+        fi;
+done
 
 
