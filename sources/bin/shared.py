ba628311470eaa780df59b8e9ef9e231b63d68bc
Solenne Correard <54953390+scorreard@users.noreply.github.com>
2026-02-10 11:09:36 +0100

    Modify retry delay in message
    Update retry delay to increase exponentially with attempts.

--- sources/bin/shared.py
+++ sources/bin/shared.py
@@ -136,7 +136,7 @@ def get_request_json(url: str, headers: dict, retries: int = 7, delay: float = 2
                 raise ConnectionError(
                     "Connection aborted after multiple retries: Remote end closed connection without response"
                 ) from e
-            print(f"Connection error on attempt {attempt}/{retries}. Retrying in {delay} seconds...")
+            print(f"Connection error on attempt {attempt}/{retries}. Retrying in {delay ** attempt} seconds...")
             time.sleep(delay ** attempt)  # Wait before retrying, wait time increases as number of attempt increases
         except requests.exceptions.RequestException as e:
             # Handles all other exceptions from the requests library
