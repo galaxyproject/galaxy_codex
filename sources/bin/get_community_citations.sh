b2af0161ff06912b2017766e37f8380e7acd5ebb
Solenne Correard <scorrear@fairwest.irisa.fr>
2026-04-09 14:00:19 +0200

    Split the resource update by community

--- sources/bin/get_community_citations.sh
+++ sources/bin/get_community_citations.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
 
-for com_data_fp in communities/* ; do
-        if [[ -d "$com_data_fp" && ! -L "$com_data_fp" ]]; then
-                community=`basename "$com_data_fp"`
-                if [[ ! -z $1  && $1 == "test"  && "$community" != "microgalaxy" ]]; then
+#for com_data_fp in communities/* ; do
+#        if [[ -d "$com_data_fp" && ! -L "$com_data_fp" ]]; then
+#                community=`basename "$com_data_fp"`
+                if [[ ! -z $1  && $1 == "test"  && "$COMMUNITY" != "microgalaxy" ]]; then
                         continue
                 fi;
 
-                if [[ "$community" != "all" && -f "communities/$community/metadata/citation_keywords" ]]; then
-                        echo "$community";
-                        mkdir -p "communities/$community/resources"
+                if [[ "$COMMUNITY" != "all" && -f "communities/$COMMUNITY/metadata/citation_keywords" ]]; then
+                        echo "$COMMUNITY";
+                        mkdir -p "communities/$COMMUNITY/resources"
                         
                         python sources/bin/citation_manager.py \
                                 filter \
                                 --all-json "communities/all/resources/citations.json" \
-                                --filtered-json "communities/$community/resources/citations.json" \
-                                --filtered-yaml "communities/$community/resources/citations.yml" \
-                                --filtered-tsv "communities/$community/resources/citations.tsv" \
-                                --keywords "communities/$community/metadata/citation_keywords"
+                                --filtered-json "communities/$COMMUNITY/resources/citations.json" \
+                                --filtered-yaml "communities/$COMMUNITY/resources/citations.yml" \
+                                --filtered-tsv "communities/$COMMUNITY/resources/citations.tsv" \
+                                --keywords "communities/$COMMUNITY/metadata/citation_keywords"
 
-                        if [[ -f "communities/$community/metadata/citations.yml" ]]; then
-                                mkdir -p _data/communities/$community/
-                                ln -sf ../../../communities/$community/resources/citations.yml _data/communities/$community/citations.yml
+                        if [[ -f "communities/$COMMUNITY/metadata/citations.yml" ]]; then
+                                mkdir -p _data/communities/$COMMUNITY/
+                                ln -sf ../../../communities/$COMMUNITY/resources/citations.yml _data/communities/$COMMUNITY/citations.yml
                         fi;
-                fi;
-        fi;
-done
+                fi
+#        fi;
+#done
